ae6f021d8502514acb2c2e4bafaef27493126508
Bogdan Rotaru <bogdan.rotaru13@yahoo.com>
2018-03-14 08:55:30 +0200

    Add semaphore

--- src/producerConsumer/Consumer.java
+++ src/producerConsumer/Consumer.java
@@ -2,25 +2,32 @@ package producerConsumer;
 
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
-//import java.util.concurrent.Semaphore;
+import java.util.concurrent.Semaphore;
 
 public class Consumer extends Thread {
 	private final Lock mutex = new ReentrantLock(true);
 
 	public void run() {
-
 		while (true) {
-			mutex.lock();
-			if (!Main.lista.isEmpty()) {
-				Main.lista.remove();
-			}
-			mutex.unlock();
-			System.out.println("A scos " + Main.lista.size());
 			try {
-				Thread.sleep(1500);
+				Main.semFull.acquire();
+				mutex.lock();
+				if (!Main.lista.isEmpty()) {
+					Main.lista.remove(0);
+				}
+				mutex.unlock();
+				Main.semFree.release();
+			} catch (InterruptedException e1) {
+				e1.printStackTrace();
+			}
+
+			System.out.println("A scos, Lista are " + Main.lista.size());
+			try {
+				Thread.sleep(3500);
 			} catch (InterruptedException e) {
 				e.printStackTrace();
 			}
+			
 
 		}
 	}
